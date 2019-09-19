1c3b97e87e88ce5ff91dab25749faddf606ab643
Roc-zhou <13031902163@163.com>
2019-09-19 10:52:37 +0800

    mongose schema

--- models/user.js
+++ models/user.js
@@ -3,21 +3,28 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: [true, 'name is required!'],
   },
   email: {
     type: String,
-    required: true
+    required: [true, 'email is required!'],
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'password is required!'],
+    validate: {
+      validator: function (v) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/.test(v);
+      },
+      message: '{VALUE} Incorrect password format'
+    }
   },
   date: {
     type: Date,
     default: Date.now
   }
 })
+// UserSchema.set('validateBeforeSave', false); 自己校验
 
 const User = mongoose.model('User', UserSchema)
 
